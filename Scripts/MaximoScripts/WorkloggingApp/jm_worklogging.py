e2584af484946a0a379d7be4f234066a1b7fd219
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-05-27 22:48:15 -0300

    Update jm_worklogging.py

--- Scripts/MaximoScripts/WorkloggingApp/jm_worklogging.py
+++ Scripts/MaximoScripts/WorkloggingApp/jm_worklogging.py
@@ -1,21 +1,64 @@
 # ------------------------------------------------------
 # MaximoCon, 2022-05-20 14:44:58-0300
-# Objetivo: Script to manipulate JM_WORKLOGGING app
+# Objetivo: Script to manipulate JM_WORKLOGGING app, relate labors and add workentrys
 # Autor: João Vitor de Oliveira Mendes
 # ------------------------------------------------------
 
 from java.util import Calendar
 from java.text import SimpleDateFormat
 
-if launchPoint == "SAVE":
+def validateLaborcodeWeeknumber():
+    laborCode = mbo.getString('JM_LABORCODE')
+    weekNumber = mbo.getString('JM_WEEKNUMBER')
+    workNumber = mbo.getString('JM_WORKNUMBER')
+
+    if weekNumber and laborCode:
+        #Where clause to get JM_WORKLOGGING with the same LABORCODE and WEEKNUMBER
+        laborMboSet = mbo.getMboSet(
+            '$JM_WORKLOGGING',
+            'JM_WORKLOGGING',
+            'JM_LABORCODE = \'' + laborCode
+            + '\' and JM_WEEKNUMBER = \'' + weekNumber
+            + '\' and JM_WORKNUMBER <> \'' + workNumber + '\'')
+
+        laborMbo = laborMboSet.moveFirst()
+
+        if laborMbo:
+            service.error('JM_WORKLOG', 'JM_WorkLogAlreadyExists')
+        else:
+            defineDays()
+
+def defineDays():
+    maxDate = mbo.getDate('JM_WEEKSTARTDATE')
+
     cal = Calendar.getInstance()
     dateFormat = SimpleDateFormat("MMM dd, YYYY");
-    maxDate = mbo.getDate('JM_WEEKSTARTDATE')
-    
+
     cal.setTime(maxDate)
-    
-    cal.add(Calendar.DATE, -1)
-    for i in range(6):
-        mbo.setValue('JM_DAY' + str(i), dateFormat.format(cal.getTime()))
+
+    #Adding date to each JM_DAY field
+    for i in range(1, 7):
+        mbo.setValue('JM_DAY' + str(i), str(dateFormat.format(cal.getTime())))
         cal.add(Calendar.DATE, 1)
-    #service.error(, "CanEditCancel")
+    #Sunday - Start at next week
+    mbo.setValue('JM_DAY0', dateFormat.format(cal.getTime()))
+
+#Save lp - add, update | before save
+if launchPoint == "SAVE":
+    if onadd:
+        validateLaborcodeWeeknumber()
+
+#Init lp
+if launchPoint == "INIT":
+    if onadd:
+        #Set laborcode as users laborcode
+        laborMboSet = mbo.getMboSet('$LABOR', 'LABOR', 'PERSONID = \'' + mbo.getUserName() + '\'')
+        laborMbo = laborMboSet.moveFirst()
+        if laborMbo and laborMbo.getString('LABORCODE'):
+            labor = laborMbo.getString('LABORCODE')
+            mbo.setValue('JM_LABORCODE',labor, mbo.NOACCESSCHECK)
+        else:
+            #If user does not have a labor code associated display error
+            service.error('JM_WORKLOG', 'JM_UserHasNoLabor')
+    else:
+        mbo.setFieldFlag('JM_WEEKNUMBER',mbo.READONLY, True)
