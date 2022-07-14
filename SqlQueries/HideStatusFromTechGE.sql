2ba204615e2e52c8b39cc6a7f9ad0ae7d7bc4e37
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-07-14 08:46:43 -0300

    Update HideStatusFromTechGE.sql

--- SqlQueries/HideStatusFromTechGE.sql
+++ SqlQueries/HideStatusFromTechGE.sql
@@ -1,2 +1,4 @@
 --Conditional experession
 :&USERNAME& not in (select userid from groupuser where UPPER(groupname) like 'SNZ_TECH%')
+--Lookup where clause
+:USER not in (select userid from groupuser where UPPER(groupname) like 'SNZ_TECH%')
