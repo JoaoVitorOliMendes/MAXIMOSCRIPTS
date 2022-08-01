5b8cc90e9d3333791747cf3949f5c7c1dea94070
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-08-01 11:22:12 -0300

    Delete Scripts/AutomationScripts/GE directory

--- Scripts/AutomationScripts/GE/GE_DIALOG_CLO_REQUEST.py
+++
@@ -1,9 +0,0 @@
-from psdi.server import MXServer
-from psdi.common.context import UIContext
-from psdi.webclient.system.controller import SessionContext, Utility, WebClientEvent
-
-mxServer = MXServer.getMXServer()
-context = UIContext.getCurrentContext()
-if context:
-    wcs = context.getWebClientSession()
-    Utility().sendEvent(WebClientEvent("GE_request_close", wcs.getCurrentPageId(), None, SessionContext(wcs)))
