c3ef31ca8bb5778a12384e34d19b47820b72d475
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-08-04 11:18:04 -0300

    Update HideNewStatusFromNonSnz.sql

--- SqlQueries/HideNewStatusFromNonSnz.sql
+++ SqlQueries/HideNewStatusFromNonSnz.sql
@@ -28,3 +28,5 @@ where domainid = 'LOCASSETSTATUS'
             and siteid = 'SNZ-MNT'
         )
     )
+
+domainid = 'LOCASSETSTATUS' and((:user not in (select userid from groupuser where UPPER(groupname) like 'SNZ%MNT' and userid not in (select userid from groupuser where UPPER(groupname) not in ('MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG') AND UPPER(groupname) not like 'SNZ%MNT')))or((:user in(select userid from groupuser where UPPER(groupname) like 'SNZ%MNT' and userid not in (select userid from groupuser where UPPER(groupname) not in ('MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG') AND UPPER(groupname) not like 'SNZ%MNT')))and orgid = 'GEROWSNZ'and siteid = 'SNZ-MNT'))
