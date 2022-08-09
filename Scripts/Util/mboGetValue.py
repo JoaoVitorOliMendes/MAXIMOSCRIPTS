df9160a730b03241770686f0d4f6ed490c5f81b1
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-08-09 14:20:30 -0300

    Create mboGetValue.py

---
+++ Scripts/Util/mboGetValue.py
@@ -0,0 +1,6 @@
+wonum = mbo.getMboValue("WONUM").getCurrentValue().asString()
+
+wonum = mbo.getMboValue("WONUM").getPreviousValue().asString()
+
+wonum = mbo.getDatabaseValue("WONUM")
+wonum = mbo.getMboValue("WONUM").getInitialValue().asString()
