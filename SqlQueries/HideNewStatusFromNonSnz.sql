1afda48272127d83d86be360687c846d068707a7
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-08-17 10:09:14 -0300

    Update HideNewStatusFromNonSnz.sql

--- SqlQueries/HideNewStatusFromNonSnz.sql
+++ SqlQueries/HideNewStatusFromNonSnz.sql
@@ -1,31 +1,63 @@
-select * from synonymdomain
-where domainid = 'LOCASSETSTATUS'
+select * from synonymdomain where
+domainid = 'LOCASSETSTATUS'
     and
     (
         (
-            :user not in 
+            'SNZ_TECH_MNT' not in
             (
-                select userid from groupuser where UPPER(groupname) like 'SNZ%MNT' and userid not in 
+                select userid from groupuser where
                 (
-                    select userid from groupuser where UPPER(groupname) not in 
-                    ('SNZ_METH_MNT', 'SNZ_TECH_MNT', 'SNZ_ADMIN_MNT','MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG')
+                    UPPER(groupname) like 'SNZ%MNT'
+                    and userid not in
+                    (
+                        select userid from groupuser where UPPER(groupname) not in ('MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG')
+                        and UPPER(groupname) not like 'SNZ%MNT'
+                    )
+                )
+                or
+                (
+                    UPPER(groupname) like 'SNZ%FM'
+                    and userid not in
+                    (
+                        select userid from groupuser where UPPER(groupname) not in ('MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG')
+                        and UPPER(groupname) not like 'SNZ%FM'
+                    )
                 )
             )
         )
         or
         (
             (
-                :user in
                 (
-                    select userid from groupuser where UPPER(groupname) like 'SNZ%MNT' and userid not in 
+                    'SNZ_TECH_MNT' in
                     (
-                        select userid from groupuser where UPPER(groupname) not in 
-                        ('SNZ_METH_MNT', 'SNZ_TECH_MNT', 'SNZ_ADMIN_MNT','MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG')
+                        select userid from groupuser where UPPER(groupname) like 'SNZ%MNT'
+                        and userid not in
+                        (
+                            select userid from groupuser where UPPER(groupname) not in ('MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG')
+                            and UPPER(groupname) not like 'SNZ%MNT'
+                        )
                     )
                 )
+                and orgid = 'GEROWSNZ'
+                and siteid = 'SNZ-MNT'
             )
-            and orgid = 'GEROWSNZ'
-            and siteid = 'SNZ-MNT'
+            or
+            (
+                (
+                    'SNZ_TECH_MNT' in
+                    (
+                        select userid from groupuser where UPPER(groupname) like 'SNZ%FM'
+                        and userid not in
+                        (
+                            select userid from groupuser where UPPER(groupname) not in ('MAXEVERYONE', 'MAXADMIN', 'MAXDEFLTREG')
+                            and UPPER(groupname) not like 'SNZ%FM'
+                        )
+                    )
+                )
+                and orgid = 'GEROWSNZ'
+                and siteid = 'SNZ-FM'
+            )
         )
     )
 
