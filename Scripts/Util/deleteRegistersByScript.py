a90381a902ea4d5e5e71dde5e6becb90b19ef201
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-08-25 11:03:47 -0300

    Update deleteRegistersByScript.py

--- Scripts/Util/deleteRegistersByScript.py
+++ Scripts/Util/deleteRegistersByScript.py
@@ -1,1 +1,11 @@
+from java.lang import System
+from java.util import Calendar
+from psdi.server import MXServer
 
+mboSet = MXServer.getMXServer().getMboSet('GE_WOGEMBA', mbo.getUserInfo())
+mbo = mboSet.moveFirst()
+while mbo:
+    if mbo.getInt('WOCOUNT') == 1:
+        mbo.delete()
+    mbo = mboSet.moveNext()
+mboSet.save()
