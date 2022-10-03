9c71e648b1ff335e7e2d1ad0c0edb1932c6633e4
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-10-03 09:28:35 -0300

    Create getTyppes.py

---
+++ Scripts/Util/getTyppes.py
@@ -0,0 +1,6 @@
+wo.getString('<FIELD>') #returns a String value
+wo.getInt('<FIELD>') #returns a Integer value
+wo.getLong('<FIELD>') #returns a BigInteger value
+wo.getDouble('<FIELD>') #returns a Double/Decimal value (better than getFloat()).
+wo.getDate('<FIELD>') #returns a Date value
+wo.getBoolean('<FIELD>') #returns Boolean Value from a YORN Field (True / False)
