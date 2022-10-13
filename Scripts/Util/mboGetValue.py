61eef9741f81817b49eb0e80ae11533e9d4f1e90
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-10-13 09:13:48 -0300

    Update mboGetValue.py

--- Scripts/Util/mboGetValue.py
+++ Scripts/Util/mboGetValue.py
@@ -4,3 +4,5 @@ wonum = mbo.getMboValue("WONUM").getPreviousValue().asString()
 
 wonum = mbo.getDatabaseValue("WONUM")
 wonum = mbo.getMboValue("WONUM").getInitialValue().asString()
+
+mbo.isModified("WONUM")
